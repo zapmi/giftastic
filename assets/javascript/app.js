dda3289038c9ac2057d61dba42075d1b1952c5b2
zapmi <mzapata2991@gmail.com>
2019-04-15 19:37:16 -0500

    everything now works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,16 +10,17 @@ $(function () {
     ];
     // show the objects in buttons array as buttons
     function showButtons() {
-        $("#buttons").empty();
+        $("#topicButtons").empty();
         for (var i = 0; i < topics.length; i++) {
             var button = $("<button>")
-            $("#buttons").append(button);
+            $("#topicButtons").append(button);
             button.attr("data-person", topics[i]);
             button.text(topics[i]);
         }
     }
+    showButtons();
 
-    $("#addAnything").on("click", function (event) {
+    $("#addTopic").on("click", function (event) {
         // event.preventDefault() prevents the form from trying to submit itself.
         // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
@@ -29,56 +30,63 @@ $(function () {
 
         // The movie from the textbox is then added to our array
         topics.push(anything);
+        
         showButtons();
+        getData();
 
     });
+    
+    
+    getData();
 
-    showButtons();
+    function getData() {
 
-    $("button").on("click", function () {
-        var person = $(this).attr("data-person");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            person + "&api_key=5l53Dh3iTD2VujKK5utYcGxKRCNJVN9n&limit=10";
+        $("button").on("click", function () {
+            var person = $(this).attr("data-person");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                person + "&api_key=5l53Dh3iTD2VujKK5utYcGxKRCNJVN9n&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var results = response.data;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>");
-                    var rating = results[i].rating;
-                    var p = $("<p>").text("Rating: " + rating);
-                    var personImage = $("<img>");
-                    personImage.attr("src", results[i].images.fixed_height_still.url);
-                    personImage.attr("data-still", results[i].images.fixed_height_still.url);
-                    personImage.attr("data-animate", results[i].images.fixed_height.url);
-                    personImage.attr("data-state", "still");
-                    personImage.attr("class", "gif");
+                    for (var i = 0; i < results.length; i++) {
+                        var gifDiv = $("<div>");
+                        var rating = results[i].rating;
+                        var p = $("<p>").text("Rating: " + rating);
+                        var personImage = $("<img>");
+                        personImage.attr("src", results[i].images.fixed_height_still.url);
+                        personImage.attr("data-still", results[i].images.fixed_height_still.url);
+                        personImage.attr("data-animate", results[i].images.fixed_height.url);
+                        personImage.attr("data-state", "still");
+                        personImage.attr("class", "gif");
 
-                    $(personImage).on("click", function () {
-                        var state = $(this).attr("data-state");
-                        console.log(state);
+                        $(personImage).on("click", function () {
+                            var state = $(this).attr("data-state");
+                            console.log(state);
 
-                        if (state == "still") {
-                            $(this).attr("src", $(this).attr("data-animate"));
-                            $(this).attr("data-state", "animate");
-                        }
-                        if (state == "animate") {
-                            $(this).attr("src", $(this).attr("data-still"));
-                            $(this).attr("data-state", "still");
-                        }
-                    });
-                    gifDiv.prepend(p);
-                    gifDiv.prepend(personImage);
+                            if (state == "still") {
+                                $(this).attr("src", $(this).attr("data-animate"));
+                                $(this).attr("data-state", "animate");
+                            }
+                            if (state == "animate") {
+                                $(this).attr("src", $(this).attr("data-still"));
+                                $(this).attr("data-state", "still");
+                            }
+                        });
+                        gifDiv.prepend(p);
+                        gifDiv.prepend(personImage);
 
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
-                // showButtons();
+                        $("#gifs-appear-here").prepend(gifDiv);
+                        
+                    }
 
-            });
-        $("#gifs-appear-here").empty();
-    });
+                });
+            $("#gifs-appear-here").empty();
+        });
+        
+    }
 });
